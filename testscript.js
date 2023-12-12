4b76e96956a00617e31338cfbfa7472a62a66601
Anna <acarlssong@hotmail.com>
2023-12-12 16:00:28 +0100

    Sortering och clearSearch()

--- testscript.js
+++ testscript.js
@@ -9,7 +9,12 @@ let teamContainer = document.querySelector("#team-container")
 let searchField = document.querySelector("#search-field")
 let searchResultList = document.querySelector("#list")
 let reserveList = document.querySelector("#reserve-list")
-let listItems = searchResultList.querySelectorAll(":scope > div")
+// let listItems = searchResultList.querySelectorAll(":scope > div")
+let teamMember1 = ''
+let teamMember2 = ''
+let teamMember3 = ''
+let nameList = []
+let a = 0
 
 tabHeaders.forEach(function(tabHeader) {
   tabHeader.addEventListener("click", function() {
@@ -41,44 +46,71 @@ function tabMyTeam(){
     tabContent2.classList.remove('hidden')
 }
 
-let listItem = document.createElement('div')
-searchResultList.appendChild(listItem)
-let listItem2 = document.createElement('div')
-searchResultList.appendChild(listItem2)
-let listItem3 = document.createElement('div')
-searchResultList.appendChild(listItem3)
-let listItem4 = document.createElement('div')
-searchResultList.appendChild(listItem4)
-let listItem5 = document.createElement('div')
-searchResultList.appendChild(listItem5)
-let listItemImage = document.createElement('img')
-// listItemImage.src = `${url.value}`
-let listItemText = document.createElement('p')
-// listItemText.innerText = 
-listItem.appendChild(listItemImage)
-listItem.appendChild(listItemText)
+searchField.addEventListener('keyup', async () => {
+    for (let i = 0; i < sample.results.length; i++){
+      if (searchField.value === ''){
+        clearSearch()
+      }
+      else
+        if (sample.results[i].name.includes(searchField.value)){
+            console.log(sample.results[i].name)
+            let url = sample.results[i].url
+          	const response = await fetch(url)
+            const data = await response.json()
+            console.log(data)
+            let listItem = document.createElement('div')
+            searchResultList.appendChild(listItem) 
+            let listItemImage = document.createElement('img')
+            listItemImage.src = data.sprites.front_default
+            let listItemText = document.createElement('p')
+            console.log('sample results', sample?.results[i])
+            listItemText.innerText = data.species.name[0].toUpperCase() + data.species.name.slice(1) //Vill gärna göra en .includes('-'),
+            //splitta vid bindestreck och versalera igen på namn med bindestreck.
+            listItem.appendChild(listItemImage)
+            listItem.appendChild(listItemText)
+            //Bugg just nu som innebär att om man söker på t.ex. 'Gourgeist' dyker 9 gourgeists upp, ett för varje knapptryck som matchar namnet.
+            //ClearSearch() i början funkar inte, eftersom det innebär att bara en Pokémon dyker upp per gång (antagligen den sista i ledet med den bokstaven),
+            //och alla andra rensas--t.ex. att söka 'o' visar bara Gourgeist och inte Koraidon, Ogerpon etc.
+        }
+    }
+})
+
+function clearSearch(){
+  let oldSearch = searchResultList.querySelectorAll('div')
+  for (let i = 0; i < oldSearch.length; i++) {
+  oldSearch[i].remove();
+}
+}
+
+// for (let i = 0; i < sample.results.length; i++){
+//   let url = sample.results[i].url
+// 	const response = await fetch(url)
+//   const data = await response.json()
+//   console.log(data)
+//   let listItem = document.createElement('div')
+//   searchResultList.appendChild(listItem) 
+//   let listItemImage = document.createElement('img')
+//   listItemImage.src = data.sprites.front_default
+//   let listItemText = document.createElement('p')
+//   console.log('sample results', sample?.results[i])
+//   listItemText.innerText = data.species.name[0].toUpperCase() + data.species.name.slice(1) //Vill gärna göra en .includes('-'),
+//   //splitta vid bindestreck och versalera igen på namn med bindestreck.
+//   listItem.appendChild(listItemImage)
+//   listItem.appendChild(listItemText)
+// }
+
+// Knapp för att lägga till i laget --
+// Sätter TeamMember1 till nuvarande Pokémon. Om TeamMember1 != null, sätt den på TeamMember2, om TeamMember2 != null, på 3 etc.
+// Om hela laget är fullt, sätt i reservlistan. Skulle också vara rätt användbart för en 'byt plats på medlemmar' funktion.
 
 // listItemImage.innerHTML = `img src=${url.value}`
 
-// searchField.addEventListener('change', () => {
-//     for (let i = 0; i < list.length; i++){
-//         if (list[i].name.includes(input.value)){
-//         nameList[a] = list[i].name
-//         a++
-//         }
-//     }
-//     for (let i = 0; i < nameList.length; i++){
-//       let newContainer = document.createElement('div')
-//       append
-//         // createElement-div per namn i namnlistan;
-//         //en div-container med två saker, img och p
-//         //innerText med namnet
-//         //appendChild till denna lista
-//     }
-// })
+//Funktion för att rensa listan;
 
 // function clearSearch(){
 //   let oldSearch = listItems.querySelectorAll('div')
+// for (let i = 0; i < oldSearch.length; i++) {
+//   oldSearch[i].remove();
 // }
 
 // nameList = []
@@ -99,3 +131,22 @@ listItem.appendChild(listItemText)
 //         //appendChild till denna lista
 //     }
 // })
+
+//Ursprunglig kod för att printa hela listan:
+
+// for (let i = 0; i < sample.results.length; i++){
+//   let url = sample.results[i].url
+// 	const response = await fetch(url)
+//   const data = await response.json()
+//   console.log(data)
+//   let listItem = document.createElement('div')
+//   searchResultList.appendChild(listItem) 
+//   let listItemImage = document.createElement('img')
+//   listItemImage.src = data.sprites.front_default
+//   let listItemText = document.createElement('p')
+//   console.log('sample results', sample?.results[i])
+//   listItemText.innerText = data.species.name[0].toUpperCase() + data.species.name.slice(1) //Vill gärna göra en .includes('-'),
+//   //splitta vid bindestreck och versalera igen på namn med bindestreck.
+//   listItem.appendChild(listItemImage)
+//   listItem.appendChild(listItemText)
+// }
