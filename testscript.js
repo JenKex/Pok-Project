822d7eb2ba4b4bbf46564feed3aad1339cf1c0d7
Anna <acarlssong@hotmail.com>
2023-12-17 21:57:49 +0100

    Varning för inkomplett lag, viss flexfix

--- testscript.js
+++ testscript.js
@@ -16,8 +16,6 @@ let teamContainer3 = document.querySelector(".member-container:last-child")
 let teamMember1 = ''
 let teamMember2 = ''
 let teamMember3 = ''
-let nameList = []
-let a = 0
 
 tabHeaders.forEach(function(tabHeader) {
   tabHeader.addEventListener("click", function() {
@@ -47,11 +45,24 @@ function tabAddPokemon(){
 function tabMyTeam(){
     tabContent1.classList.add('hidden')
     tabContent2.classList.remove('hidden')
+    checkTeamComplete()
 }
 
 //Det känns som att jag har fått fler problem med dubletter?
 //Måste implementera dublett-protection, är alldeles för bängligt annars om man skriver snabbt
 //Gör också ett alfabet och verifiera att keyup-value matchar alfabetet, blir annars så att listan refreshar vid shift, ctrl, etc
+
+//För cacheande av hela funktionen skulle jag kunna göra på ett annat sätt -- fetcha hela APIn, sedan skapa en lista objekt i LocalStorage
+//med formen 'namn, image, ability', hämta de från relevanta delar av APIn och sätta in det som en komplett lista.
+//Detta skulle bli galet många requests på en gång -- runt 2000? -- men sedan inte behöva göra fler requests alls, och det känns annars som att
+//det blir alldeles för mycket requests med sökfunktionen om inget sparas.
+//Men man kanske kan göra en kombination: Fetcha+cachea hela APIt i början. Sätt in en söksträng. Kolla vilka namn den söksträngen matchar. Om de
+//finns i objektlistan, dra infon därifrån, om inte dra den ifrån APIt och sätt in den i objektlistan? Så att istället för att det blir
+//sök på 'omanyte' -- api fetch
+//sök på 'omanyte' -- api fetch
+//blir det
+//sök på 'omanyte' -- api fetch, spara relevanta variabler som ett objekt
+//sök på 'omanyte' -- dra ut relevanta variabler från objektlista?
 
 searchField.addEventListener('keyup', async () => {
   clearSearch()
@@ -83,13 +94,67 @@ searchField.addEventListener('keyup', async () => {
             console.log(listItemText.innerText)
             listItemAddButton.addEventListener('click', () =>{
               if (teamMember1 === ''){
+                //Kan säkert göra det här istället med en for-loop och hitta member-container med nth-child[i] istället för att definiera i början?
+                //Men tycker å andra sidan att det är rätt bra att ha en bakgrunds-teamMember(x) variabel att kunna matcha villkor mot.
                 teamMember1 = listItemText.innerText
+                let memberName = teamContainer1.querySelector('.poke-name')
+                let memberImage = teamContainer1.querySelector('.poke-image')
+                let buttonContainer = teamContainer1.querySelector('.buttons')
+                let buttonRemove = document.createElement('button')
+                buttonRemove.addEventListener('click', () =>{
+                  memberName.innerText = ''
+                  memberImage.src = ''
+                  teamMember1 = ''
+                  buttonRemove.remove()
+                  checkTeamComplete()
+                })
+                // I mån av tid, implementera en knapp som kickar till reservlistan och drar en reserv därifrån;
+                // let buttonReserve = document.createElement('button')
+                // buttonReserve.addEventListener('click', () =>{
+
+                // })
+                memberName.innerText = listItemText.innerText
+                memberImage.src = listItemImage.src
+                buttonRemove.innerText = 'Remove'
+                buttonContainer.appendChild(buttonRemove)
               }
               else if (teamMember2 === ''){
                 teamMember2 = listItemText.innerText
+                let memberName = teamContainer2.querySelector('.poke-name')
+                let memberImage = teamContainer2.querySelector('.poke-image')
+                let buttonContainer = teamContainer2.querySelector('.buttons')
+                let buttonRemove = document.createElement('button')
+                buttonRemove.addEventListener('click', () =>{
+                  memberName.innerText = ''
+                  memberImage.src = ''
+                  teamMember2 = ''
+                  buttonRemove.remove()
+                  checkTeamComplete()
+                })
+                // let buttonRelease = document.createElement('button')
+                memberName.innerText = listItemText.innerText
+                memberImage.src = listItemImage.src
+                buttonRemove.innerText = 'Remove'
+                buttonContainer.appendChild(buttonRemove)
               }
               else if (teamMember3 === ''){
                 teamMember3 = listItemText.innerText
+                let memberName = teamContainer3.querySelector('.poke-name')
+                let memberImage = teamContainer3.querySelector('.poke-image')
+                let buttonContainer = teamContainer3.querySelector('.buttons')
+                let buttonRemove = document.createElement('button')
+                buttonRemove.addEventListener('click', () =>{
+                  memberName.innerText = ''
+                  memberImage.src = ''
+                  teamMember3 = ''
+                  buttonRemove.remove()
+                  checkTeamComplete()
+                })
+                // let buttonRelease = document.createElement('button')
+                memberName.innerText = listItemText.innerText
+                memberImage.src = listItemImage.src
+                buttonRemove.innerText = 'Remove'                
+                buttonContainer.appendChild(buttonRemove)
               }
               else{
                 let reserveListItem = document.createElement('div')
@@ -100,6 +165,10 @@ searchField.addEventListener('keyup', async () => {
                 let reserveListItemText = document.createElement('p')
                 reserveListItemText.innerText = listItemText.innerText
                 let reserveListItemRemoveButton = document.createElement('button')
+                reserveListItemRemoveButton.addEventListener('click', () =>{
+                  reserveListItem.remove()
+                })
+                reserveListItemRemoveButton.innerText = 'Remove'
                    // Sätt innertext, image etc. baserat på Pokémonen som valdes ut.
                 reserveListItem.appendChild(reserveListItemImage)
                 reserveListItem.appendChild(reserveListItemInfoContainer)
@@ -136,11 +205,90 @@ function clearSearch(){
 }
 }
 
+function checkTeamComplete(){
+  if (teamMember1 === '' || teamMember2 === '' || teamMember3 === ''){
+    if (teamContainer.querySelector('h3')){
+      
+    }
+    else{
+      let incompleteTeam = document.createElement('h3')
+      incompleteTeam.innerText = 'Your team needs at least three Pokémon.'
+      teamContainer.appendChild(incompleteTeam)
+    }
+  }
+  else{
+    if (!teamContainer.querySelector('h3')){
+
+    }
+    else{
+    let incompleteTeam = teamContainer.querySelector('h3')
+    incompleteTeam.remove()
+    }
+  }
+}
+
+// Denna funktionen kommer nu smygfuska -- den lägger till den nya Pokémonen i team-containern med sitt vanliga namn, och ändrar sedan
+// innertexten på detta namn efter det läggs till. Borde kunna göra om det genom att bara köra AddPokemon efter att antingen ett 'yes'
+// eller 'no' har tryckts (vilket också skulle lösa möjliga problem med att behöva leta i reservlistan), men vill först se om det här funkar
+// alls. Om jag kan lösa mitt problem med variabler vill jag också gärna dela upp alla dessa i funktioner -- nicknameAdd(), addPokemon(), etc.,
+// vilket borde göra blocken mer läsbara.  
+
+//function nicknameAdd(){
+  // listItemImage.remove()
+  // listItemInfoContainer.remove()
+  // let nicknamePrompt = document.createElement('p')
+  // let nicknamePrompt.innerText 'Would you like to add a nickname?'
+  // let yesButton = document.createElement('button')
+  // let noButton = document.createElement('button')
+  // yesButton.innerText = 'Yes'
+  // noButton.innerText = 'No'
+  // yesButton.addEventListener('click', () =>{
+    // nicknamePrompt.remove()
+    // yesButton.remove()
+    // noButton.remove()
+  // let nicknameInput = document.createElement('input')
+  // let confirmButton = document.createElement('button')
+  // confirmButton.innerText = 'Confirm'
+  // confirmButton.addEventListener('click', () =>{
+    // if (teamMember1 === ''){
+    // let nickname = teamContainer1.querySelector('.poke-name')
+    // nickname.innerText = nicknameInput.value
+    // }
+    // else if (teamMember2 === ''){
+    // let nickname = teamContainer2.querySelector('.poke-name')
+    // nickname.innerText = nicknameInput.value
+    // }
+    // else if (teamMember3 === ''){
+    // let nickname = teamContainer3.querySelector('.poke-name')
+    // nickname.innerText = nicknameInput.value
+    // }
+//    listItem.appendChild(listItemImage)
+//    listItem.appendChild(listItemInfoContainer)
+//    listItemInfoContainer.appendChild(listItemInfoText)
+//    listItemInfoContainer.appendChild(listItemAddButton)
+//  })
+//  })
+  // noButton.addEventListener('click', () =>{
+    // nicknamePrompt.remove()
+    // yesButton.remove()
+    // noButton.remove()
+//    listItem.appendChild(listItemImage)
+//    listItem.appendChild(listItemInfoContainer)
+//    listItemInfoContainer.appendChild(listItemInfoText)
+//  listItemInfoContainer.appendChild(listItemAddButton)
+//  })
+// listItem.appendChild(nicknamePrompt)
+// listItem.appendChild(yesButton)
+// listItem.appendChild(noButton)
+//}
+
 //OBS -- när jag lade till teamMember1 = listItemText.innerText i eventlistenern direkt var det inga problem, men när jag sätter in den
 //i en funktion och sedan kallar på den funktionen får jag felet 'listItemText is not defined'. Detta känns underligt för mig, eftersom
 //det känns rätt rimligt -- den drar ifrån listItemText som assignas sist--men det är samma formulering, bara inkallad från en funktion istället...
-//Kanske behöver ligga till hela funktionen -- alltså 'button addeventlistener' i en separat funktion för att det ska funka? Eller
-//skulle det bara leda till 'button is undefined'? 
+//Kanske behöver lägga till hela funktionen -- alltså 'button addeventlistener' i en separat funktion för att det ska funka? Eller
+//skulle det bara leda till 'button is undefined'? Känns definitivt som ett scope-problem, men vet inte hur man ska fixa det, eftersom
+//att declarea en createElement först i dokumentet bara leder till komplikationer. Går det att declarea till ett helt tomt värde?
+//Ja -- kanske skulle gå om jag bara skrev 'let listItemText, listItemImage' etc.
 
 //function addPokemon(){
   // if (teamMember1 === ''){
@@ -174,6 +322,7 @@ function clearSearch(){
 
 //Egentligen känns det inte nödvändigt att ha alla dessa reserveListItem-etc. variabler i och med att informationen finns i det man just har skapat?
 //Kan bara sätta en 'reserveList.appendChild(listitem)' etc?
+//I och för sig -- behöver fortfarande skapa ett nytt element för reservlistan, och inte dubbel-appenda det för ursprungslistan.
 
 // function addToReserve(){
 //   let reserveListItem = document.createElement('div')
